fded2d093a29fca90f830959eb1553fed7cc6960
umardeveloper <umar.abduganiyev@yandex.ru>
2021-09-30 08:07:13 -0700

    haha qottimi

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// function sum(arrNumbers = [4, 5, 6, 6,]){
+//     var result = 0;
+//     for(var number of arrNumbers){
+//         result= result + number;
+//     }
+//     return result;
+// }
+// console.log(sum());
+
+
+for(var i = 0; true; i++){
+    console.log(i);
+}
